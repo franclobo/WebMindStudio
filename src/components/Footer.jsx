4a40bd1f0b9876a89d4aa3dde98b09183e15b88b
franclobo <fjbl2788@gmail.com>
2023-09-12 15:41:39 -0500

    Edit address

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,11 +22,11 @@ function Footer() {
           src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7979.628185580849!2d-78.5037583!3d-0.0834076!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sec!4v1694549433002!5m2!1ses!2sec"
           width="250"
           height="250"
-          style="border:0;"
-          allowfullscreen=""
+          style={{ border: "0" }}
+          allowFullScreen=""
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
-          title="Web Minds Studio"
+          title="Parroquia San Judas Tadeo"
         />
       </article>
       <article className="footer__article">
@@ -46,15 +46,15 @@ function Footer() {
         <h3 className="footer__title">Contacto</h3>
         <p className="footer__text">
           <PhoneOutlined />
-          Celular: +593 961 842 276
+          Celular: +593 961-842-276
         </p>
         <p className="footer__text">
           <MailOutlined />
-          Email: sanjudastadeolaroldos@gmail.com
+          Email: fjbl2788@gmail.com
         </p>
         <p className="footer__text">
           <ClockCircleOutlined />
-          Horario de atención: 15:00 - 18:00 de miércoles a sábado
+          Horario de atención: 08:00 - 18:00 de Lunes a Sábado
         </p>
       </article>
     </footer>
