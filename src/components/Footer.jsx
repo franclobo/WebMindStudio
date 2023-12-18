e587fd3291b875ee2ce3ae51216407b8303e244c
mundoempleabilidad <mundoempleabilidad@gmail.com>
2023-12-18 09:19:23 -0500

    Edit address

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,7 +19,7 @@ function Footer() {
           Oe7B N86-67 y Francisco de Rumihurco, El Condado.
         </p>
         <iframe
-          src="https: //www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.814090789583!2d-78.5063822259608!3d-0.08342733549234653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d585c4a3e7029d%3A0xb5afedad806898b0!2sWebMinds%20Studio!5e0!3m2!1ses-419!2snl!4v1702908612646!5m2!1ses-419!2snl"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.814090789583!2d-78.5063822259608!3d-0.08342733549234653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d585c4a3e7029d%3A0xb5afedad806898b0!2sWebMinds%20Studio!5e0!3m2!1ses-419!2snl!4v1702908612646!5m2!1ses-419!2snl"
           width="250"
           height="250"
           style={{ border: "0" }}
@@ -46,13 +46,17 @@ function Footer() {
         <h3 className="footer__title">Contacto</h3>
         <p className="footer__text">
           <PhoneOutlined />
-          Celular: +56 9 2758 3663
+          Celular: <br />
+          Quito: +593 96 184 2276
           <br />
-          +56 9 3065 8893
+          Santiago de Chile: +56 9 3065 8893
         </p>
         <p className="footer__text">
           <MailOutlined />
-          Email: francisco.borja@webmindsstudio.com
+          Email:
+          <br />
+          francisco.borja@webmindsstudio.com
+          <br />
           carlos.chiluisa@webmindsstudio.com
         </p>
         <p className="footer__text">
